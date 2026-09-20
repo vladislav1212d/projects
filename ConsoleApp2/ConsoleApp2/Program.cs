37c1efd72013eb892d441eade706e5c06959cdc3
vladislav1212d <vladi_penev@abv.bg>
2026-09-20 14:12:33 +0300

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,22 @@
+﻿    string favouriteBook = Console.ReadLine();
+    string searchBook = Console.ReadLine();
+    int count = 0;
+    bool found = false;
+
+    for (string book = Console.ReadLine(); book != "No More Books"; book = Console.ReadLine())
+    {
+        count++;
+
+        if (book == searchBook)
+        {
+            Console.WriteLine($"You checked {count} books and found it.");
+            found = true;
+            break;
+        }
+    }
+
+    if (!found)
+    {
+        Console.WriteLine("The book you search is not here!");
+        Console.WriteLine($"You checked {count} books.");
+    }
